ba4bb15edb82295337265b555a21ff509bf3d152
abdurrahmangrahadim <hadi@busanid.dev>
2024-02-20 01:21:22 +0700

    fixed bug

--- resources/views/customer/menuDetail.blade.php
+++ resources/views/customer/menuDetail.blade.php
@@ -6,11 +6,13 @@
         <h2><span>Menu</span> Detail</h2>
 
         <div class="row">
-            <img src="{{ asset('product') .'/'. $item->thumbnail }}" alt="{{ $item->name }}" className="img-fluid" width="300">
+            <img src="{{ asset('product') . '/' . $item->thumbnail }}" alt="{{ $item->name }}" className="img-fluid"
+                width="300">
             <div class="content" style="padding-left: 25px;">
-                <h3>{{ $item->name }}</h3>
-                <p>{{ "Rp." .number_format($item->price, 2, ",", ".") }}</p>
-                 </div>
+                <h3>{{ $item->name }} - <span style="text-decoration:underline;">{{ $item->category->name }}</span></h3>
+                <p>{{ 'Rp.' . number_format($item->price, 2, ',', '.') }}</p>
+                <div style="font-size:1.2rem;margin-top:1rem;">{!! $item->desc !!}</div>
+            </div>
         </div>
     </section>
     <!-- Menu Detail section end-->
